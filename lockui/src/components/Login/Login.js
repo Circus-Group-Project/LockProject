cd16f413f609d5e107478524b14e07be926ef78d
Nirmith Victor <nirmithdalmeida@hotmail.com>
2023-07-11 14:38:34 -0400

    added UI for registration and login

---
+++ lockui/src/components/Login/Login.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Login.css'
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div className='LoginComponent'>
+      <form className='LoginForm'>
+        <h1>
+          <span>Login</span>
+        </h1>
+        <div>
+            <label>UserName</label>
+            <input className='inpBox'
+            type="text"
+            placeholder="Enter your user name"
+            required
+            />
+        </div>
+        <div>
+            <label>Password</label>
+            <input className='inpBox'
+            type="password"
+            placeholder="Enter your password"
+            required
+            />
+        </div>
+        <div className="submitBtn">
+            <button type="submit">Submit</button>
+        </div>
+        <h3>New to the site? <Link to="/register"><span>Register</span></Link></h3>
+      </form>
+    </div>
+  )
+}
+
+export default Login
---
+++ lockui/src/routes/RegistrationRoute.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+import Registration from '../components/registration/Registration'
+
+const RegistrationRoute = () => {
+  return (
+    <>
+      <Navbar/>
+      <Registration/>
+    </>
+  )
+}
+
+export default RegistrationRoute
