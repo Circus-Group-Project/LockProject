0447d65e31579d77492e12f653d8125d7f0b920b
Nirmith Victor <nirmithdalmeida@hotmail.com>
2023-07-13 20:23:44 -0400

    add a temporary page and dialogs

---
+++ lockui/src/routes/AdminProfileRoute.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Navbar from '../components/Navbar/Navbar';
+import Profile from '../components/Profile/AdminProfile';
+
+const ProfileRoute = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Profile></Profile>
+    </>
+  )
+}
+
+export default ProfileRoute
